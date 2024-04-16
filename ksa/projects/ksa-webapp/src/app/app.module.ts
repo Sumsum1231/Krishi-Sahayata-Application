b833a8d61981e677d1255c3dc0143fb88b4dd89e
Sumsum1231 <sumitdash1971@gmail.com>
2024-04-17 00:07:30 +0530

    feat: KSA-Home-Page

---
+++ ksa/projects/ksa-webapp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeatureCardComponent } from './feature-card/feature-card.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FeatureCardComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
