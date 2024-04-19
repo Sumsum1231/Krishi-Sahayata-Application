5e7c7b6fffe611c82969643849e4c0d00ef5eaff
om-666 <omtanmaya2004@gmail.com>
2024-04-19 14:25:02 +0530

    Feat:About Implemented with odia language support

--- ksa/projects/ksa-webapp/src/app/about-page/about-page.component.ts
+++ ksa/projects/ksa-webapp/src/app/about-page/about-page.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './about-page.component.css'
 })
 export class AboutPageComponent {
+  isEnglishDisplayed: boolean = true;
+
+  translate(): void {
+    this.isEnglishDisplayed = !this.isEnglishDisplayed;
+  }
+
+  getButtonText(): string {
+    return this.isEnglishDisplayed ? 'ଓଡ଼ିଆ' : 'English';
+  }
 
 }
